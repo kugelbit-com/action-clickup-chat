0cdf737b03aba3f46cd7a2d04358317c718278ad
Giovanni Silva <gsilva-git@kugelbit.com>
2025-03-24 22:16:49 -0300

    first version

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import * as core from "@actions/core";
+import axios, { AxiosRequestConfig } from "axios";
+
+export async function run(): Promise<void> {
+  try {
+    const apiKey = core.getInput("key");
+    const typeOfMessage = core.getInput("type");
+    const format = core.getInput("format");
+    const content = core.getInput("content");
+    const channel = core.getInput("channel");
+    const workspaceId = core.getInput("workspaceId");
+    const apiDomain = core.getInput("apiUrl");
+    const apiUrl = `${apiDomain}/api/v3/workspaces/${workspaceId}/chat/channels/${channel}/messages`;
+    const headers = {
+      Authorization: apiKey
+    };
+
+    const requestBody = {
+      type: typeOfMessage,
+      content_format: format,
+      content: content
+    };
+
+    const requestBodyJson = JSON.stringify(requestBody);
+    console.log(`Seding request to channel ${channel}`);
+
+    const config: AxiosRequestConfig = {
+      url: apiUrl,
+      method: "POST",
+      headers: headers,
+      data: requestBodyJson
+    };
+
+    const response = await axios(config);
+    core.setOutput("response-status", response.status.toString());
+    const responseData = JSON.stringify(response.data);
+    core.setOutput("response-data", responseData);
+
+    core.setOutput("messageId", response.data?.id);
+    core.info(`API request succeeded with status code ${response.status}`);
+  } catch (error: any) {
+    core.setFailed(`API request failed: ${error.message}`);
+  }
+}
